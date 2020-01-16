10f9abf6d58fa089a9a9146665265921ecd543c6
heejuShin <21800412@handong.edu>
2020-01-16 14:25:26 +0900

    spring project and readme

--- JSP/HelloSpring/src/main/java/com/handong/csee/HomeController.java
+++ JSP/HelloSpring/src/main/java/com/handong/csee/HomeController.java
@@ -4,12 +4,16 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -23,7 +27,7 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public ModelAndView home(Locale locale, ModelAndView mv) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -31,9 +35,31 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
+		mv.addObject("serverTime", formattedDate);
+		mv.setViewName("home");
 		
-		return "home";
+		//model.addAttribute("serverTime", formattedDate );
+		
+		return mv;
 	}
 	
+	@RequestMapping(value = "/nextpage", method = RequestMethod.POST)
+	public ModelAndView nextpage(ModelAndView mv, @RequestParam("input_name") String input_name) {
+		mv.setViewName("nextpageview");
+		mv.addObject("name", input_name);
+		
+		System.out.println("input name: "+input_name);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value = "/nextpage2", method = RequestMethod.POST)
+	public ModelAndView nextpage2(ModelAndView mv, HttpServletRequest request) {
+		mv.setViewName("nextpageview");
+		mv.addObject("name", request.getParameter("input_name"));
+		
+		System.out.println("input name: "+request.getParameter("input_name"));
+		
+		return mv;
+	}
 }
